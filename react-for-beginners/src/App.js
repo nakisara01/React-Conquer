d62e8285087f8321417a45c57d19e293f02974c9
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-05 02:10:08 +0900

    feat: toDoList App

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,71 +1,38 @@
 import { useState, useEffect } from "react";
 
-// function App() {
-//   const [counter, setValue] = useState(0);
-//   const [keyword, setKeyword] = useState("");
-//   const onClick = () => setValue((prev) => prev + 1);
-//   const onChange = (event) => setKeyword(event.target.value);
-
-//   useEffect(() => {
-//     if (keyword !== "" && keyword.length > 5) {
-//       console.log("i run when 'keyword' changed. (over 5 letters)");
-//     }
-//   }, [keyword]);
-
-//   useEffect(() => {
-//     console.log("i run only one time")
-//   }, [])
-
-//   useEffect(() => {
-//     if (counter > 0) {
-//       console.log("i run when 'counter' is changed")
-//     }
-//   }, [counter])
-
-//   useEffect(() => {
-//     if (counter > 0 || keyword !== "") {
-//       console.log("i run when 'counter' & 'keyword' is changed")
-//     }
-//   }, [counter, keyword])
-
-//   return (
-//     <div>
-//       <input
-//         value={keyword}
-//         onChange={onChange}
-//         type="text"
-//         placeholder="Search here..."
-//       />
-//       <h1>{counter}</h1>
-//       <button onClick={onClick}>click me</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  useEffect(() => {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...toDos]);
+  };
+  console.log(toDos);
+
+  console.log(toDo);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          paceholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
