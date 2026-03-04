8a3181ad62391ad81213a3d858664dc055ab596c
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-04 17:04:20 +0900

    feat: useEffects

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,44 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("i run when 'keyword' changed. (over 5 letters)");
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("i run only one time")
+  }, [])
+
+  useEffect(() => {
+    if (counter > 0) {
+      console.log("i run when 'counter' is changed")
+    }
+  }, [counter])
+
+
+  useEffect(() => {
+    if (counter > 0 || keyword !== "") {
+      console.log("i run when 'counter' & 'keyword' is changed")
+    }
+  }, [counter, keyword])
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
