278bb104a31ccdffd67d8d69d47156fd761854ae
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-05 18:38:47 +0900

    feat: setTimeout

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,42 +1,14 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("construct");
-  }
-
   state = {
-    count: 0,
+    isLoading: true,
+    movies: []
   };
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  Minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-
   componentDidMount() {
-    console.log("component rendered");
-  }
-  componentDidUpdate() {
-    console.log("component updated");
-  }
-  componentWillUnmount() {
-    console.log("component Unmounted");
-  }
-
-  render() {
-    console.log("render");
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.Minus}>Minus</button>
-      </div>
-    );
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
 
   render() {
