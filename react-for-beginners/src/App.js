00b4defcabbc3e3c22f21b8ce388cd52aed5932f
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-05 10:55:07 +0900

    feat: coin Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,38 +1,57 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((responce) => responce.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const [amount, setAmount] = useState();
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...toDos]);
+    setAmount("");
   };
-  console.log(toDos);
 
-  console.log(toDo);
   return (
     <div>
-      <h1>My To Dos {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          paceholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1> The Coins {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}{" "}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <hr />
+          <h1>Bitcoin Exchanger</h1>
+          <form onSubmit={onSubmit}>
+            <input
+              value={amount}
+              id="cash"
+              placeholder="write your budget"
+              type="number"
+              onChange={onChange}
+            />
+            <button>exchange</button>
+          </form>
+          <h3>{amount/72660}</h3>
+        </div>
+      )}
     </div>
   );
 }
