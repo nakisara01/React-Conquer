596e682fb2e8d8e79d523b6a7e529746342d062e
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-06 17:03:59 +0900

    Hook: useInput custom Hook

--- hookhook/src/App.js
+++ hookhook/src/App.js
@@ -1,17 +1,33 @@
 import React, { Component, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(prev => prev + 1);
-  const decrement = () => setCount(prev => prev - 1);
-  const 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
 
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+function App() {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrease</button>
+      <h1>hi</h1>
+      <input placeholder="name" {...name} />
     </div>
   );
 }
