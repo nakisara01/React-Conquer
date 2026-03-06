b91c1ef4915280d121f26d0020a3a8752aa9d362
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-06 17:42:10 +0900

    Hook: useTabs custom Hook

--- hookhook/src/App.js
+++ hookhook/src/App.js
@@ -1,33 +1,40 @@
 import React, { Component, useState } from "react";
 import "./App.css";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    
-    const {
-      target: { value },
-    } = event;
-    let willUpdate = true;
-    
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
 
-    if (willUpdate) {
-      setValue(value);
-    }
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
   };
-  return { value, onChange };
 };
 
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
+
 function App() {
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
+
   return (
     <div>
       <h1>hi</h1>
-      <input placeholder="name" {...name} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 }
