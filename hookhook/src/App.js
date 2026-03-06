e3acd502aafc3dcc15a0de90aa7c17d3d053458c
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-06 18:15:31 +0900

    Hook: useClick custom Hook (useEffect)

--- hookhook/src/App.js
+++ hookhook/src/App.js
@@ -1,42 +1,29 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const useTabs = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex,
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
   };
+  useEffect(updateTitle, [title]);
+  return setTitle;
 };
 
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1",
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2",
-  },
-];
-
 function App() {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const titleUpdater = useTitle("Loading");
+  setTimeout(() => {
+    titleUpdater("Home")
+  }, 3000)
 
   return (
     <div>
       <h1>hi</h1>
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
     </div>
   );
 }
 
 export default App;
+
+//useEffect는 ComponentDidMount, ComponentWillUnMount, ComponentDidUpdate이다
